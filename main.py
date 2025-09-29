b57b498f260c80f0320a019767693fa2c1604624
hamza14596 <hmzuuu87@gmail.com>
2025-09-29 08:59:28 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import pygame
+import sys
+
+from scripts.entity import PhysicsEntity
+from scripts.utils import load_image
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        
+
+        self.screen = pygame.display.set_mode((1280,760))
+        pygame.display.set_caption("The Ninja's Beginning")
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0,0,0))
+
+        self.img_pos = [160,260]
+        self.movement = [False,False]
+
+        self.assets = {
+            'player' : load_image('entities/player.png')
+        }
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
+        self.player = PhysicsEntity(self, 'player', (50,50), (8, 15))
+
+    def run(self):
+       
+        while True:
+            
+            self.display.fill((14,219,248))
+            
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                        
+
+                
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()),(0,0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
