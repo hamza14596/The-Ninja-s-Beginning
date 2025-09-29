53c5602a69dbbbd46bd200a40be132c0783f6ac9
hamza14596 <hmzuuu87@gmail.com>
2025-09-29 12:16:33 +0500

    added background, coud animations and jump

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -4,4 +4,5 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-        .
+        for i in range(10):
+            self.tilemap[str(3 + i) + ';10'] = {'type' : 'grass', 'variant' : 1, 'pos': (3 + i, 10)}
