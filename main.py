e9fa41e7c38081a23a906f25a8f84289b30bfd37
hamza14596 <hmzuuu87@gmail.com>
2025-09-29 12:18:01 +0500

    addition

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pygame
 import sys
 
 from scripts.entity import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 
 class Game:
@@ -24,21 +26,48 @@ class Game:
         self.movement = [False,False]
 
         self.assets = {
-            'player' : load_image('entities/player.png')
+            'decor' : load_images('tiles/decor'),
+            'grass' : load_images('tiles/grass'),
+            'large_decor' : load_images('tiles/large_decor'),
+            'stone' : load_images('tiles/stone'),
+            'player' : load_image('entities/player.png'),
+            'background' :load_image('background.png'),
+            'clouds' : load_images('clouds')
         }
+
+        self.clouds = Clouds(self.assets['clouds'], count = 16)
 
         self.collision_area = pygame.Rect(50, 50, 300, 50)
 
-        self.player = PhysicsEntity(self, 'player', (50,50), (8, 15))
+        self.player = PhysicsEntity(self, 'player', (100, 100), (16, 16))
+
+
+        self.tilemap = Tilemap(self, tile_size = 16)
+
+        self.scroll = [0,0]
 
     def run(self):
        
         while True:
             
-            self.display.fill((14,219,248))
+            self.display.blit(self.assets['background'], (0,0))
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) /30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+
             
-            self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset = render_scroll)
+
+            self.tilemap.render(self.display, offset = render_scroll)
+            
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display, offset=render_scroll)
+
+
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -49,6 +78,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
